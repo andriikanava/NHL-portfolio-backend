9549c997fe74bcca7620e6a332991deea3a31333
Andrii Kanava <andrii.kanava@student.nhlstenden.com>
2025-11-18 16:55:32 +0100

    add User to admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, User
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'url')
     search_fields = ('title',)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email')
+    search_fields = ("username",)
