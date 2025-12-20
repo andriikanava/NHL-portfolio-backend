dd948d1b76509bb75a604e81372bfc3e08c814b7
Andrii Kanava <andrii.kanava@student.nhlstenden.com>
2025-12-20 15:03:59 +0100

    Add status for comments and files models

--- core/models/comment.py
+++ core/models/comment.py
@@ -4,10 +4,13 @@ from .project import Project
 from .user import User
 
 class Comment(BaseModel):
+    STATUS_CHOICES = [("OPEN", "open"), ("CLOSE", "close")]
+
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     comment = models.TextField(null=False, blank=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="comments")
+    status = models.CharField(choices=STATUS_CHOICES)
 
 
     def __str__(self):
-        return f"{self.name} → {self.project}"
+        return f"{self.comment} → {self.project}"
--- core/models/media.py
+++ core/models/media.py
@@ -3,10 +3,13 @@ from .base import BaseModel
 from .project import Project
 
 class UploadedFile(BaseModel):
-    file = models.ImageField(upload_to="uploads/")
+    STATUS_CHOICES = [("APPROVED", "approved"), ("REJECTED", "rejected"), ("SUBMITTED", "submitted")]
+
+    file = models.FileField(upload_to="uploads/")
     original_name = models.CharField(max_length=255)
     file_type = models.CharField(max_length=10)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="files")
+    status = models.CharField(choices=STATUS_CHOICES)
 
     def __str__(self):
         return f"{self.original_name} → {self.project}"
